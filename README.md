0a9177f6c07a35145210c6b56b24837727df9f1d
Patrick Fuller <patrickfuller@gmail.com>
2015-07-24 16:34:08 -0500

    Rewrote to remove IgH dependency and add async. Fixes #1 and #2.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 mks
 ===
 
-#####This driver is currently untested. Use with caution.
+#####This driver is under heavy development. Use with caution.
 
 Python driver and command-line tool for [MKS EtherCAT mass flow controllers](http://www.mksinst.com/product/category.aspx?CategoryID=406).
 
@@ -12,39 +12,14 @@ Python driver and command-line tool for [MKS EtherCAT mass flow controllers](htt
 Installation
 ============
 
-This driver currently depends on [IgH EtherCAT Master](http://www.etherlab.org/en/ethercat/).
-To install this dependency, clone the repo, install
-[this patch](http://lists.etherlab.org/pipermail/etherlab-dev/2014/000435.html),
-build, and register an ethernet port for use.
-
-```
-hg clone http://hg.code.sf.net/p/etherlabmaster/code ethercat-hg
-cd ethercat-hg
-# Patch, if needed
-./bootstrap linux
-./configure --disable-8139too
-make
-make modules
-sudo insmod master/ec_master.ko main_devices=[mac address]  # Get mac from e.g. `ifconfig`
-sudo chmod 666 /dev/EtherCAT0
-sudo insmod devices/ec_generic.ko
-sudo ifconfig eth[X] up
-```
-
-If you encounter issues, consult [this forum post](http://lists.etherlab.org/pipermail/etherlab-dev/2014/000368.html).
-Test with:
-
-```
-dmesg
-ethercat --help
-ethercat pdos
-```
-
-Once this is working, you can install this library with:
-
 ```
 pip install git+https://github.com/numat/mks
 ```
+
+When the driver is more stable, we will release on PyPI.
+
+Usage
+=====
 
 ###Command Line
 
@@ -71,23 +46,67 @@ $ mks 2 --set 1
 
 See `mks --help` for more.
 
-###Python
+###Python (Asynchronous)
 
-For more complex behavior, you can write a python script to interface with
-other sensors and actuators.
+Asynchronous programming allows us to send out all of our requests in parallel, and then
+handle responses as they trickle in. For more information, read through
+[krondo's twisted introduction](http://krondo.com/?page_id=1327).
 
 ```python
 from mks import FlowController
+from tornado.ioloop import IOLoop, PeriodicCallback
+
+def on_response(response):
+    """This function gets run whenever a device responds."""
+    print(response)
+
+def loop():
+    """This function will be called in an infinite loop by tornado."""
+    flow_controller.get(on_response)
+
 flow_controller = FlowController(position=1)
-print(flow_controller.get())
+
+PeriodicCallback(loop, 500).start()
+IOLoop.current().start()
 ```
 
-If the controller is operating at that address, this should output a
-dictionary of the form:
+This looks more complex, but the advantages are well worth it at scale.
+Essentially, sleeping is replaced by scheduling functions with tornado. This
+allows your code to do other things while waiting for responses.
 
-```python
-{
-  'flow': 0.1  # Current flow rate, in units specified by device model
-  'setpoint': 0.1  # Setpoint flow rate, also in units specified by device model
-}
+Extra Resources
+===============
+
+[IgH EtherCAT Master](http://www.etherlab.org/en/ethercat/) is a fully featured
+etherCAT tool, and can be used to debug new devices. It's not required for this
+driver to function, but it'll help if you're having problems. However, it's a
+pain to install.
+
+If you want to install, ctlone the repo (the mercurial repo *may* be down), install
+[this patch](http://lists.etherlab.org/pipermail/etherlab-dev/2014/000435.html),
+build, and register an ethernet port for use.
+
 ```
+hg clone http://hg.code.sf.net/p/etherlabmaster/code ethercat-hg
+cd ethercat-hg
+# Patch
+./bootstrap linux
+./configure --disable-8139too
+make
+make modules
+sudo insmod master/ec_master.ko main_devices=[mac address]  # Get mac from e.g. `ifconfig`
+sudo chmod 666 /dev/EtherCAT0
+sudo insmod devices/ec_generic.ko
+sudo ifconfig eth[X] up
+```
+
+If you encounter issues, consult [this forum post](http://lists.etherlab.org/pipermail/etherlab-dev/2014/000368.html).
+Test with:
+
+```
+dmesg
+ethercat --help
+ethercat pdos
+```
+
+It requires kernel headers, and won't install readily on e.g. raspberry pis.
--- mks/__init__.py
+++ mks/__init__.py
@@ -1,69 +1,263 @@
 #!/usr/bin/python
 """
-A Python driver for MKS' etherCAT flow controllers.
+A Python driver for MKS etherCAT flow controllers.
 
 Distributed under the GNU General Public License v2
 Copyright (C) 2015 NuMat Technologies
 """
+import binascii
+import fcntl
 import logging
-from subprocess import call, check_output
+import random
+import socket
+import struct
+import traceback
+
+import tornado.ioloop
+import tornado.iostream
 
 
 class FlowController(object):
     """Python driver for [MKS EtherCAT mass flow controllers](http://www.
     mksinst.com/product/category.aspx?CategoryID=406).
-
-    This driver is a simple command-line wrapper around IgH EtherCAT master.
-    This will change in a future version.
     """
-    def __init__(self, position=0):
-        """Specifies etherCAT position and tests connection."""
-        self.position = position
-        self._get_flow_command = ['ethercat', 'upload', '-p',
-                                  str(self.position), '0x6000', '0x01']
-        self._get_setpoint_command = ['ethercat', 'upload', '-p',
-                                      str(self.position), '0x7003', '0x01']
-        self._set_setpoint_command = ['ethercat', 'download', '-p',
-                                      str(self.position), '0x7003', '0x01']
-        self._check_command = ['ethercat', 'slaves', '-p', str(self.position)]
-        self.retries = 0
-        self._check()
+    dest = 'ffffffffffff'
+    ethertype = '88a4'
+    ethercat_header = '9a10'
+    working_count = '0000'
+    packet_size = 170
+    upload = '40'
+    download = '23'
+    prep_template = ('07{i}{pos}3001028000000000000005{j}{next}'
+                     '0010800000000a00010000030020{command}{pdo}{size}{data}')
+    run_template = '07{i}{pos}3001028000000000000004{j}{next}001480'
 
-    def get(self):
-        """Gets the current and setpoint flow of the device."""
-        flow = check_output(self._get_flow_command)
-        setpoint = check_output(self._get_setpoint_command)
-        return {'flow': float(flow), 'setpoint': float(setpoint)}
+    def __init__(self, position=0, interface='eth0'):
+        """Connects a raw socket to an interface, and wraps with tornado."""
+        self.socket = socket.socket(socket.PF_PACKET, socket.SOCK_RAW,
+                                    socket.htons(int(self.ethertype, 16)))
+        self.socket.bind((interface, 0))
+        self.stream = tornado.iostream.IOStream(self.socket)
 
-    def set(self, setpoint):
-        """Sets the setpoint flow, and checks to guarantee set."""
-        call(self._set_setpoint_command + [str(setpoint)])
-        current_setpoint = check_output(self._get_setpoint_command)
-        if abs(setpoint - float(current_setpoint)) < 1e-3:
-            self.retries = 0
-        elif self.retries >= 3:
-            self.retries = 0
-            raise IOError("Can not set flow.")
-        else:
-            logging.info("Flow setpoint not updated. Retrying.")
-            self.retries += 1
-            self.set(setpoint)
+        self.position = self._flip('{:04x}'.format(position))
+        self.next_position = self._flip('{:04x}'.format(position + 1))
+        self.interface = interface
+        self.source = self._get_mac()
+        self.response_source = '{:x}'.format(int(self.source, 16) + 2 ** 41)
+        self.ethernet_header = self.dest + self.source + self.ethertype
+        self.template = (self.ethernet_header + self.ethercat_header +
+                         '{payload}' + self.working_count)
+        self.setting_flow = False
+        self.ioloop = tornado.ioloop.IOLoop.current()
 
-    def _check(self):
-        """Checks that an MKS controller is communicating."""
-        result = check_output(self._check_command)
-        if not result:
-            raise IOError("No device found at position {:d}.".format(
-                          self.position))
-        name = result.decode('ascii').split(maxsplit=4)[-1]
-        if 'EtherCAT MFC' not in name:
-            raise IOError("MFC not found at position {:d}. Current device is"
-                          " '{}'.".format(self.position, name))
+    def get(self, callback):
+        """Returns the setpoint and actual flow rates as a dictionary.
+
+        This method calls `get_flow` and `get_setpoint` in tandem. If you
+        only need one of the two values, use these methods instead.
+        """
+        def flow_callback(response):
+            """After flow is retrieved, call `get_setpoint`."""
+            self.get_setpoint(lambda r: setpoint_callback(r, response))
+
+        def setpoint_callback(setpoint, flow):
+            """After setpoint is retrieved, fire user's callback."""
+            callback({'flow': flow, 'setpoint': setpoint})
+
+        self.get_flow(flow_callback)
+
+    def get_flow(self, callback):
+        """Gets the current flow rate."""
+        self._handle_communication('6000', '01', self.upload, callback)
+
+    def get_setpoint(self, callback):
+        """Gets the setpoint flow rate."""
+        self._handle_communication('7003', '01', self.upload, callback)
+
+    def set(self, setpoint, retries=3):
+        """Sets the setpoint flow rate.
+
+        This method requests a lock on the socket, deferring all other
+        requests until a value has been set.
+        """
+        def on_setpoint():
+            """After setting the flow, remove the lock and request a check."""
+            self.setting_flow = False
+            self.get_setpoint(check_setpoint)
+
+        def check_setpoint(response):
+            """Check that the flow controller's setpoint matches target."""
+            if abs(setpoint - response) < 0.01:
+                logging.debug("Set flow setpoint to {:.2f}.".format(response))
+            elif retries <= 0:
+                raise IOError("Could not set setpoint.")
+            else:
+                logging.debug("Could not set setpoint. Retrying...")
+                self.set(setpoint, retries=retries-1)
+
+        self.setting_flow = True
+        self._handle_communication('7003', '01', self.download,
+                                   on_setpoint, data=setpoint)
+
+    def _handle_communication(self, pdo, size, command, callback, data=None):
+        """Communicates with the EtherCAT device to retrieve data.
+
+        This is the bulk of the driver, and was thrown together by reverse
+        engineering etherCAT frames sent from IgH EtherCAT master. By removing
+        the need for kernel modifications, this should be more portable.
+
+        Largely, the etherCAT data is treated as a binary payload wrapped in
+        an ethernet frame. By doing this, we lose many of the benefits of
+        etherCAT (e.g. fast response times, packet minimization).
+        However, these benefits are negligible considering the internal
+        mechanical delays of flow controllers.
+
+        Some precautions were made to ensure proper functionality. Requests
+        follow a req-res-req-res pattern, and will restart the chain when an
+        error is detected. Set the loglevel to debug to watch more.
+        """
+        def defer():
+            """Re-calls the function after a slight delay."""
+            self.ioloop.call_later(0.05, lambda: self._handle_communication(
+                                    pdo, size, command, callback, data))
+
+        if self.setting_flow and command == self.upload:
+            logging.debug("Request for data deferred while setting flow.")
+            defer()
+            return
+
+        index = random.randint(10, 240)
+
+        def send_preparation_request():
+            """The first request. Appears to prepare the device for I/O."""
+            kwargs = {'pdo': self._flip(pdo), 'pos': self.position,
+                      'size': size, 'i': '{:02x}'.format(index),
+                      'j': '{:02x}'.format(index + 1), 'command': command,
+                      'data': binascii.hexlify(struct.pack('<f', data))
+                      if data else '', 'next': self.next_position}
+            ethercat = self.prep_template.format(**kwargs)
+            prep_command = self.template.format(payload=self._pad(ethercat))
+            self.stream.write(binascii.unhexlify(prep_command))
+
+            if self.stream.reading():
+                logging.debug("Stream busy. Restarting request.")
+                defer()
+                return
+            else:
+                self.stream.read_bytes(self.packet_size,
+                                       on_prepared_confirmation)
+
+        def on_prepared_confirmation(result):
+            """Checks the device reply and continues if okay.
+
+            The device takes the etherCAT frame and flips some bits. Working
+            counters are incremented, slave positions are incremented,
+            and one of the source MAC address bits is flipped. We're assuming
+            one-device communication, so we can test this behavior to ensure
+            we have the right packet.
+            """
+            res = binascii.hexlify(result)
+            try:
+                self._check(res, index)
+            except ValueError:
+                logging.debug("Malformed response: " + traceback.format_exc())
+                send_preparation_request()
+                return
+            send_data_request()
+
+        def send_data_request():
+            """The second request. Appears to ask for data."""
+            ethercat = self.run_template.format(pos=self.position,
+                                                next=self.next_position,
+                                                i='{:02x}'.format(index + 4),
+                                                j='{:02x}'.format(index + 5))
+            run_command = self.template.format(payload=self._pad(ethercat))
+            self.stream.write(binascii.unhexlify(run_command))
+            if self.stream.reading():
+                logging.debug("Stream busy. Restarting request.")
+                defer()
+                return
+            else:
+                self.stream.read_bytes(self.packet_size, on_data_received)
+
+        def on_data_received(result):
+            """Retrieves device data and fires user callbacks.
+
+            If we asked for data, this response will have it.
+            """
+            res = binascii.hexlify(result)
+            try:
+                self._check(res, index + 4)
+            except ValueError:
+                logging.debug("Malformed response: " + traceback.format_exc())
+                send_data_request()
+                return
+
+            received_pdo = self._flip(res[98:102])
+            # This occurs when the request happens faster than the update
+            if received_pdo != pdo:
+                send_data_request()
+            elif command == self.upload:
+                callback(struct.unpack('!f', bytes(result[52:56][::-1]))[0])
+            elif command == self.download:
+                callback()
+
+        send_preparation_request()
+
+    def _get_mac(self):
+        """Returns the MAC address of the bound interface."""
+        info = fcntl.ioctl(self.socket.fileno(), 0x8927,
+                           struct.pack('256s', self.interface[:15]))
+        return binascii.hexlify(info[18:24])
+
+    def _check(self, response, index):
+        """A random series of sanity checks for the etherCAT packet."""
+        if len(response) != self.packet_size * 2:
+            raise ValueError("Improper packet size. This driver is hardcoded "
+                             "to expect a size of {:d}."
+                             .format(self.packet_size))
+        if any((response[:12] != self.dest,
+                response[12:24] != self.response_source,
+                response[24:28] != self.ethertype,
+                response[28:32] != self.ethercat_header)):
+            received = ':'.join((response[:12], response[12:24],
+                                 response[24:28], response[28:32]))
+            expected = ':'.join((self.dest, self.response_source,
+                                 self.ethertype, self.ethercat_header))
+            raise ValueError("Malformed headers. Received '{}', but expected "
+                             "'{}'.".format(received, expected))
+        if int(response[34:36], 16) != index:
+            raise ValueError("EtherCAT index mismatch. Wrong packet received.")
+
+    def _flip(self, data):
+        """Flips the endianness of a hex byte representation.
+
+        This driver uses 0-f ascii characters as its internal representation
+        of binary. It's twice the size of pure bytes, but easier to debug. We
+        lose the benefit of struct packing (e.g. < vs. > for endianness), so
+        we need to write our own.
+        """
+        return ''.join([data[i:i+2] for i in range(0, len(data), 2)][::-1])
+
+    def _pad(self, ethercat_data):
+        """Pads the etherCAT frame with zeroes.
+
+        From the IgH etherCAT master frames, we set a minimum size for the
+        etherCAT frames. The controller gets nowhere near this size, so we
+        have plenty of room.
+
+        TODO This fixed-size padding approach could be eliminated easily.
+        """
+
+        pad_size = self.packet_size - 18 - len(ethercat_data) / 2
+        return ethercat_data + '0' * 2 * pad_size
 
 
 def command_line():
     import argparse
     import json
+    from json import encoder
+    encoder.FLOAT_REPR = lambda o: format(o, '.2f')
 
     parser = argparse.ArgumentParser(description="Control an MKS etherCAT MFC "
                                      "from the command line.")
@@ -75,9 +269,18 @@ def command_line():
     args = parser.parse_args()
 
     flow_controller = FlowController(args.position)
+
+    ioloop = tornado.ioloop.IOLoop.current()
+
+    def print_flows(flows):
+        """Prints the flows and stops async loop once data is retrieved."""
+        print(json.dumps(flows, indent=2, sort_keys=True))
+        ioloop.stop()
+
     if args.set is not None:
         flow_controller.set(args.set)
-    print(json.dumps(flow_controller.get(), indent=2, sort_keys=True))
+    flow_controller.get(print_flows)
+    ioloop.start()
 
 
 if __name__ == '__main__':
