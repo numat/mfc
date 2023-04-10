8f3beed45ccaa0b665e4ca32f10b23d5745384f3
Alex Ruddick <a.ruddick@numat-tech.com>
2023-04-10 18:14:57 -0500

    Fix broken version check

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 """Python driver for MKS mass flow controllers."""
-from platform import python_version
 from setuptools import setup
 
-if python_version() < '3.5':
-    raise ImportError("This module requires Python >=3.5")
-
-with open('README.md', 'r') as in_file:
+with open('README.md') as in_file:
     long_description = in_file.read()
 
 setup(
@@ -14,7 +10,7 @@ setup(
     description="Python driver for MKS mass flow controllers.",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url="http://github.com/numat/mfc/",
+    url="https://github.com/numat/mfc/",
     author="Patrick Fuller",
     author_email="pat@numat-tech.com",
     packages=['mfc'],
